09d147dabee1e4e542b8966b09e93f02bd5e8a1f
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 20:50:45 +0700

    db: add create customers table migration file

---
+++ migrations/1722433285064-create_customers_table.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCustomersTable1722433285064 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'identity'
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            length: '20',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('customers')
+  }
+}
