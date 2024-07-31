c3847b8841d2ab3cb7070fcdeb1df5d7d1f1f24d
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 19:37:48 +0700

    refactor: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from '@app/app.controller';
-import { AppService } from '@app/app.service';
 import { ItemsModule } from '@app/items/items.module';
 import typeorm from '@app/config/typeorm';
 
@@ -20,7 +18,5 @@ import typeorm from '@app/config/typeorm';
     }),
     ItemsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
