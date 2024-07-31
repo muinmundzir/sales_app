146c1f8aeb5c1e098258798f7aa0206be65a4f34
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 19:51:04 +0700

    feat: add create item endpoint

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 import { ItemsService } from '@app/items/items.service';
+import { CreateItemDto } from '@app/items/dto/create-item.dto';
 
+@ApiTags('Items')
 @Controller('items')
 export class ItemsController {
   constructor(private itemsService: ItemsService) {}
 
+  @ApiOkResponse({
+    description: 'Return list of items',
+  })
   @Get('')
   async listItems() {
     return await this.itemsService.find();
+  }
+
+  @ApiCreatedResponse({
+    description: 'Return created item',
+  })
+  @ApiBody({
+    type: CreateItemDto,
+    description: 'JSON structure for trip object',
+  })
+  @Post('create')
+  async createItem(@Body() itemDto: CreateItemDto) {
+    return await this.itemsService.create(itemDto)
   }
 }
