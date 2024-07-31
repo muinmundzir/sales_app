bb6f261fa89102b18ff3f2cebc9e9a03f5f1f1aa
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 19:52:07 +0700

    squash! squash! feat: add create item endpoint

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -3,11 +3,27 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Item } from '@app/items/item.entity';
+import { CreateItemDto } from '@app/items/dto/create-item.dto';
 
 @Injectable()
 export class ItemsService {
   constructor(@InjectRepository(Item) private itemsRepository: Repository<Item>) {
 
+  }
+
+  async create(itemDto: CreateItemDto) {
+    try {
+      const { code, name, price } = itemDto
+      
+      const item = new Item()
+      item.code = code;
+      item.name = name;
+      item.price = price;
+
+      return await this.itemsRepository.save(item)
+    } catch (error) {
+      throw error;
+    }
   }
 
   async find() {
