646240cda8fdf1a67839ed58ea65e18d10fb276b
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 21:26:41 +0700

    feat: add find one customer endpoint

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiBody, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 
 import { CustomersService } from '@app/customers/customers.service';
 import { Customer } from '@app/customers/customer.entity';
@@ -28,5 +28,20 @@ export class CustomersController {
   @Post('create')
   async createItem(@Body() itemDto: CreateCustomerDto) : Promise<Customer> {
     return await this.customersService.create(itemDto)
+  }
+
+  @ApiOkResponse({
+    description: 'Return one customer',
+  })
+  @ApiNotFoundResponse({
+    description: 'Customer with inserted ID not found'
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'number'
+  })
+  @Get('/:id')
+  async getItem(@Param() itemQuery: { id: number }) : Promise<Customer> {
+    return await this.customersService.findOne(itemQuery.id)
   }
 }
--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { Customer } from '@app/customers/customer.entity';
-import { CreateCustomerDto } from './dto/create-customer.dto';
+import { CreateCustomerDto } from '@app/customers/dto/create-customer.dto';
 
 @Injectable()
 export class CustomersService {
@@ -26,6 +26,18 @@ export class CustomersService {
   async find() : Promise<Customer[]> {
     try {
       return await this.customersRepository.find();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findOne(customerId: number) : Promise<Customer> {
+    try {
+      const customer = await this.customersRepository.findOneBy({ id: customerId });
+
+      if(!customer) throw new NotFoundException('Customer not found')
+
+      return customer
     } catch (error) {
       throw error;
     }
