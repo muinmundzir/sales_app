d101046af17276402916b6241fdd2b8c48fb7814
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-07-31 20:52:39 +0700

    feat: add customer entity

---
+++ src/customers/customer.entity.ts
@@ -0,0 +1,29 @@
+
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('customers')
+export class Customer {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  name: string;
+
+  @Column()
+  phone: string;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
