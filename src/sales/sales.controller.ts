4c5511c9efdf0aea8df8cd4d545363a4b1995718
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-01 10:17:47 +0700

    feat: add get all sales endpoint

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
+import { SalesService } from '@app/sales/sales.service';
+import { Sale } from '@app/sales/sale.entity';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Sales')
 @Controller('sales')
-export class SalesController {}
+export class SalesController {
+  constructor(private salesService: SalesService) {}
+
+  @ApiOkResponse({
+    description: 'Return list of sales',
+  })
+  @Get('')
+  async getAll(): Promise<Sale[]> {
+    return await this.salesService.find();
+  }
+}
--- src/sales/sales.service.ts
+++ src/sales/sales.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Sale } from '@app/sales/sale.entity';
 
 @Injectable()
-export class SalesService {}
+export class SalesService {
+  constructor(
+    @InjectRepository(Sale) private salesRepository: Repository<Sale>
+  ) {}
+
+  async find(): Promise<Sale[]> {
+    try {
+      return await this.salesRepository.find();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
