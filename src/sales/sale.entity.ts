36c37a9b88417fd7bcdb8ebf1bf8b9af86c92fc7
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-01 10:11:47 +0700

    feat: add sale entity

---
+++ src/sales/sale.entity.ts
@@ -0,0 +1,57 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Customer } from '@app/customers/customer.entity';
+
+@Entity('sales')
+export class Sale {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column({
+    type: 'timestamp',
+  })
+  date: Date;
+
+  @Column()
+  subtotal: number;
+
+  @Column({ name: 'customer_id' })
+  customerId: number;
+
+  @Column()
+  discount: number;
+
+  @Column({ name: 'shipping_cost' })
+  shippingCost: number;
+
+  @Column({ name: 'total_payment' })
+  totalPayment: number;
+
+  @Column({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @Column({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => Customer, (customer) => customer.sales)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer[];
+}
