3c0cd5e51dbab6c483295ca599c5f67cee644f6e
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-01 10:17:29 +0700

    feat: register sale entity

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { SalesService } from './sales.service';
-import { SalesController } from './sales.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { SalesService } from '@app/sales/sales.service';
+import { SalesController } from '@app/sales/sales.controller';
+import { Sale } from '@app/sales/sale.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sale])],
   providers: [SalesService],
-  controllers: [SalesController]
+  controllers: [SalesController],
 })
 export class SalesModule {}
