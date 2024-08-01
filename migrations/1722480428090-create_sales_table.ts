97283d4bc63dbd44a5ef3f9663fdfcb1903ee640
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-01 10:10:57 +0700

    db: add create sales table migration file

---
+++ migrations/1722480428090-create_sales_table.ts
@@ -0,0 +1,85 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateSalesTable1722480428090 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'sales',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'identity',
+          },
+          {
+            name: 'customer_id',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'code',
+            type: 'varchar',
+            length: '15',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp',
+            isNullable: false,
+          },
+          {
+            name: 'subtotal',
+            type: 'numeric',
+            isNullable: true,
+          },
+          {
+            name: 'discount',
+            type: 'numeric',
+            isNullable: true,
+          },
+          {
+            name: 'shipping_cost',
+            type: 'numeric',
+            isNullable: true,
+          },
+          {
+            name: 'total_payment',
+            type: 'numeric',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'sales',
+      new TableForeignKey({
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('sales');
+  }
+}
