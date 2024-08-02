75e3a4e0d25536b0e203ee1bdec55ba711856455
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-02 18:30:20 +0700

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import { swaggerConfig } from '@app/config/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       exceptionFactory: (errors) => {
@@ -20,7 +22,6 @@ async function bootstrap() {
     })
   );
 
-  app.enableCors();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api-docs', app, document);
