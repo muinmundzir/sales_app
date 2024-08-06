00e0d712b687fef75108a5c20b40e42352803b6f
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2024-08-06 12:35:06 +0700

    feat: add alternatif configuration for db connection

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -7,7 +7,7 @@ dotenvConfig({ path: '.env' });
 const config = {
   type: `${process.env.DB_TYPE}`,
   host: `${process.env.DB_HOST}`,
-  // port: `${process.env.DB_PORT}`,
+  port: `${process.env.DB_PORT}`,
   username: `${process.env.DB_USERNAME}`,
   password: `${process.env.DB_PASSWORD}`,
   database: `${process.env.DB_NAME}`,
@@ -15,10 +15,18 @@ const config = {
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: false,
-  // ssl: {
-  //   rejectUnauthorized: true // or false if you don't want to reject unauthorized certificates
-  // }
 };
 
-export default registerAs('typeorm', () => config);
+const connectionConfig = process.env.DB_URL
+  ? {
+      type: 'postgres', // or other types if needed
+      url: process.env.DB_URL,
+      entities: ['dist/src/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: false,
+    }
+  : config;
+
+export default registerAs('typeorm', () => connectionConfig);
 export const connectionSource = new DataSource(config as DataSourceOptions);
